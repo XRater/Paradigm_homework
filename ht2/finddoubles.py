b55770414d10151d0a980a46abe00c3adef0b33b
XRater <kair1998@gmail.com>
2016-09-17 23:16:58 +0300

    Find_double_complited

--- ht2/finddoubles.py
+++ ht2/finddoubles.py
@@ -1,13 +1,30 @@
 ﻿import sys, os
+from hashlib import md5
 
 def func(root_folder):
+	d = {}
 	for root, dirs, files in os.walk(root_folder):
-		print(root, "consumes", end=" ")
 		for f in files:
-			print(root + '/' + f, end = ', ')
-		print()
-	#	print(sum(getsize(join(root, name)) for name in files), end=" ")
-	#	print("bytes in", len(files), "non-directory files")
+			if f[0] == '~' or f[0] == '.':
+				continue;
+			path = root + '\\' + f
+			with open(path, "br") as newf:
+				content = newf.read()
+			m =  md5()
+			m.update(content)
+			key = m.hexdigest()
+			if d.get(key, 0):
+				d[key].append(path)
+			else:
+				d[key] = [path]
+
+	for key, value in d.items():
+		if len(value) > 1:
+			print(value.pop(), end = '')
+			for f in value:
+				print(':'+ f, end = '')
+			print()            
+
 
 
 def main(): 
@@ -19,3 +36,4 @@ def main():
 	   	
 if __name__ == '__main__':
 	main()
+        
