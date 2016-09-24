7a72575e3bad9acf2e135162a77227c0d656f58f
XRater <kair1998@gmail.com>
2016-09-24 21:57:09 +0300

    Fix3

--- ht3/schtrassen.py
+++ ht3/schtrassen.py
@@ -4,22 +4,24 @@ def get_matrix(n):
 	arr = []
 	for i in range (n):
 		arr.append(input().split())
-	mat_a = np.array(arr, dtype = int)
-	return(mat_a)                         
+	mat_a = np.matrix(arr, dtype = int)	  
+	return mat_a                         
 
-def resize_matrix(mat_a, n):
-	k = 1
-	while k < n:
-		k *= 2 
-	mat_a = np.hstack((mat_a,  np.zeros((n, k - n), dtype = int)))
-	mat_a = np.vstack((mat_a,  np.zeros((k - n, k), dtype = int)))  
-	return (mat_a, k)
+def resize_matrix(mat_a, size):
+	n = mat_a.shape[0]
+	if (size > n):
+		mat_a = np.hstack((mat_a,  np.zeros((n, szie - n), dtype = mat_a.dtype)))
+		mat_a = np.vstack((mat_a,  np.zeros((size - n, size), dtype = mat_a.dtype)))  
+		return mat_a
+	else: 
+		mat_a = mat_a[:size, :size]
+		return mat_a
 
-def mult_matrix(mat_a, mat_b, n):	
-	if n == 1:
+def mult_matrix(mat_a, mat_b):	
+	if mat_a.shape[0] == 1:
 		return mat_a*mat_b
 	else:
-		n = n // 2
+		n = mat_a.shape[0] // 2
 		a11 = mat_a[:n, :n]
 		a12 = mat_a[:n, n:]
 		a21 = mat_a[n:, :n]
@@ -28,40 +30,40 @@ def mult_matrix(mat_a, mat_b, n):
 		b12 = mat_b[:n, n:]
 		b21 = mat_b[n:, :n]
 		b22 = mat_b[n:, n:]
-
-		mat_1 = mult_matrix(a11 + a22, b11 + b22, n)
-		mat_2 = mult_matrix(a21 + a22, b11, n)
-		mat_3 = mult_matrix(a11, b12 - b22, n)
-		mat_4 = mult_matrix(a22, b21 - b11, n)
-		mat_5 = mult_matrix(a11 + a12, b22, n)
-		mat_6 = mult_matrix(a21 - a11, b11 + b12, n)
-		mat_7 = mult_matrix(a12 - a22, b21 + b22, n)
-						
+		mat_1 = mult_matrix(a11 + a22, b11 + b22)
+		mat_2 = mult_matrix(a21 + a22, b11)
+		mat_3 = mult_matrix(a11, b12 - b22)
+		mat_4 = mult_matrix(a22, b21 - b11)
+		mat_5 = mult_matrix(a11 + a12, b22)
+		mat_6 = mult_matrix(a21 - a11, b11 + b12)
+		mat_7 = mult_matrix(a12 - a22, b21 + b22)						
 		c11 = mat_1 + mat_4 - mat_5 + mat_7
 		c21 = mat_2 + mat_4
 		c12 = mat_3 + mat_5
 		c22 = mat_1 + mat_3 - mat_2 + mat_6
-
-		c = np.zeros((2 * n, 2 * n), dtype = int) 
+		c = np.zeros((mat_a.shape[0], mat_a.shape[0]), dtype = mat_a.dtype) 
 		c[:n, :n] = c11
 		c[:n, n:] = c12
 		c[n:, :n] = c21
 		c[n:, n:] = c22
 		return c
 
-def print_matrix(matrix, size):
-
-	for row in matrix[:size, :size]:
+def print_matrix(matrix):
+	for row in matrix:
 		print(' '.join(map(str, row)))     
 
 def main(): 
 	real_size = int(input())
 	mat_a = get_matrix(real_size)
 	mat_b = get_matrix(real_size)
-	mat_a, size = resize_matrix(mat_a, real_size)
-	mat_b, size = resize_matrix(mat_b, real_size)
-	mat_c = mult_matrix(mat_a, mat_b, size)	
-	print_matrix(mat_c, real_size)
+	size = 1
+	while size < real_size:
+		size *= 2 
+	mat_a = resize_matrix(mat_a, size)
+	mat_b = resize_matrix(mat_b, size)
+	mat_c = mult_matrix(mat_a, mat_b)
+	mat_c = resize_matrix(mat_c, real_size)	
+	print_matrix(mat_c)
 	
 
 if __name__ == '__main__':
